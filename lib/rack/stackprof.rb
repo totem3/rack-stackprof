3e805e09064556638658a0cb25c50aeba99d5c4f
totem3 <takhirata6@gmail.com>
2019-02-16 21:56:34 +0900

    Added an option to filter request path to profile

--- lib/rack/stackprof.rb
+++ lib/rack/stackprof.rb
@@ -2,11 +2,29 @@ require 'stackprof'
 require 'rack/stackprof/version'
 
 class Rack::Stackprof
+  # @param [Rack::Application] app
+  # @param [Hash] options
+  # @option options [Integer] :profile_interval_seconds
+  # @option options [Integer] :sampling_interval_microseconds
+  # @option options [String] :result_directory The directory to save the profiling results.
+  # @option options [String] :profile_include_path
+  #   Request paths to save profile. If this option is not nil nor empty,
+  #   requests only matching the path are profiled.
+  #   This is must be String which is valid as a Regexp, or Regexp
   def initialize(app, options = {})
     @app = app
     @profile_interval_seconds = options.fetch(:profile_interval_seconds)
     @sampling_interval_microseconds = options.fetch(:sampling_interval_microseconds)
     @last_profiled_at = nil
+    include_path = options.fetch(:profile_include_path)
+    @profile_include_path = case include_path
+                            when Regexp
+                              include_path
+                            when String
+                              if !include_path.empty?
+                                Regexp.compile(include_path)
+                              end
+                            end
     StackProf::Middleware.path = options.fetch(:result_directory) # for `StackProf::Middleware.save`
   end
 
@@ -20,12 +38,16 @@ class Rack::Stackprof
   private
 
   def profile_every(seconds:, env:, &block)
-    if @last_profiled_at.nil? || @last_profiled_at < Time.now - seconds
+    if should_profile?(env) && (@last_profiled_at.nil? || @last_profiled_at < Time.now - seconds)
       @last_profiled_at = Time.now
       with_profile(env) { block.call }
     else
       block.call
     end
+  end
+
+  def should_profile?(env)
+    @profile_include_path.nil? || env['PATH_INFO'].match(@profile_include_path)
   end
 
   def with_profile(env)
